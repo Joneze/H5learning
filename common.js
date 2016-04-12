8c89898e9ae1c5605eb9c587fd6041e7ec5e10d9
yingshitoutiao <zenghui198964@gmail.com>
2016-04-12 11:01:43 +0800

    H5学习JS demo
    JavaScript DOM 编程的艺术 第四章 学习demo

---
+++ common.js
@@ -0,0 +1,14 @@
+function showPic (whichpic) {
+	var source = whichpic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src",source);
+	
+	var text = whichpic.getAttribute("title");
+	var description  = document.getElementById("description");
+	description.firstChild.nodeValue = text;
+}
+
+function countBodyChildren () {
+	var body_element = document.getElementsByTagName("body")[0];
+	alert(body_element.childNodes.length);
+}
